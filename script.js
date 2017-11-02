8cb43a4c739a55107abaa12be98a57f6a519ecde
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2017-11-02 23:46:54 +0000

    pseudo code is done with the code below it, but it's not working

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(function(event) {
   console.log('DOM is ready');
 
-//reset button has to return to original variables (if I declare global variables, will have to call them again, follow calculator logic)
-//var count will be counting everytime a button is clicked, if it reaches 9, we will have a tie
-  var player = true;
+  //reset button has to return to original variables (if I declare global variables, will have to call them again, follow calculator logic)
+  //var count will be counting everytime a button is clicked, if it reaches 9, we will have a tie
+  var moveOne = true;
+  var moveTwo = false;
   var count = 1;
 
 
-// use Math.floor(Math.random), to get X or O -------------- I can also use a variable that goes between the 2 values
-
+  // function to randomly obtain X or O
   function xOrO() {
     if (Math.floor(Math.random() * 2) < 1) {
       console.log('X');
@@ -21,6 +21,7 @@ $(function(event) {
   var input = xOrO();
 
 
+  //
   $('td').on('click', function(){
     console.log("I've been clicked");
     $(this).html(input);
@@ -29,39 +30,30 @@ $(function(event) {
     } else {
       $(this).addClass('O');
     }
-
+    count ++;
   })
-
-// 
-// function game() {
-//   while (true) {
-//
-//   }
-// }
+  // debugger
 
 
-// if (count === 9) {
-//  alert("It's a tie!");
-// }
-
-//winning options
-
-//
-// funtion winning
-//
-//
-//
-//
-//
-// var winningOptions = [[2,4,6], [6,7,8], [0,1,2], [0,4,8], [3,4,5], [0,3,6], [2,5,8], [1,4,7]];
-//
-// if (winningOptions = true) {
-//   alert ('Victoriaaaa');
-// } else if ( === 9 ) {
-//   alert ("It's a tie!")
-// }
+  // for each click, store the data-num into an array
+  function winningOptions() {
+    //store click button information into an array
+    $('td').each(function(index, square){
+      $(this).attr('data-num');
+      var conditions = [];
+    });
+  }
+  winningOptions();
 
 
 
 
+  //Array of winning options
+  var conditions = [[2,4,6], [6,7,8], [0,1,2], [0,4,8], [3,4,5], [0,3,6], [2,5,8], [1,4,7]];
+
+  if (conditions = true) {
+    alert ('Victoriaaaa');
+  } else if ( count === 9 ) {
+    alert ("It's a tie!")
+  }
 });
